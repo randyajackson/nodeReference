d88de07d749fc11517ffae0e95402af83fd8082f
randyajackson <rajackson.dev@gmail.com>
2019-04-21 17:38:19 -0600

    http module

--- Node Module System/app_http.js
+++ Node Module System/app_http.js
@@ -1,13 +1,31 @@
 const http = require('http');
 
-const server = http.createServer();
+//const server = http.createServer();
 //this server is an event emitter
 //server.on
 //server.emit
 
-server.on('connection', (socket) => {
-    console.log('new connection');
+//this code lets you know a connection is being made
+// server.on('connection', (socket) => {
+//     console.log('new connection');
+// });
+
+//instead of working with a socket, we are working with a request and the repsonse objects
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello');
+        res.end();
+    }
+
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
 });
+
+//not using http module to make a backend.
+//use a framework called express
+//built on top of HTTP module
 
 server.listen(3000);
 
