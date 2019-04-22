dca5199b1ab97ee2c20fb253fdf8567cee1451d0
randyajackson <rajackson.dev@gmail.com>
2019-04-21 18:43:24 -0600

    npm using package

---
+++ Node Package Manager (NPM)/index.js
@@ -0,0 +1,11 @@
+const _ = require('underscore');
+
+//require checks for module in 3 places
+//core module
+//File or folder
+//node_modules
+
+var result = _.contains([1,2,3], 2);
+console.log(result);
+
+
---
+++ Node Package Manager (NPM)/notes_npm.js
@@ -0,0 +1,30 @@
+/**
+ * Node Package Manager (NPM)
+ * open source libraries
+ * npmjs.com
+ * 
+ * npm -v
+ * version
+ * 
+ * npm i -g npm@5.5.1
+ * installs npm version 5.5.1 for tutorials
+ * 
+ * before adding any node packages
+ * need to create a package.json file in the directory
+ * json file that includes basic information about the project
+ * 
+ * cd to directory and run 
+ * npm init
+ * 
+ * can run
+ * npm init --yes
+ * 
+ * npm module is found in load_modules library and added to package.json
+ * 
+ * more in index.js
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
